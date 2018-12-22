84e31e64f3b5ed846353a25d8a28f6eb28431b49
HP <outmane.inf@gmail.com>
2018-12-22 12:38:59 +0100

    First app in git

---
+++ resources/views/home.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h2>Image Gallery</h2>
+ 
+  
+  <div class="row">
+    @foreach($imgurl as $imag)
+    <div class="col-md-4">
+
+      <div class="thumbnail">
+      	<div class="hovereffect">
+        <a href="{{$imag->URL}}" target="_blank">
+          <img class="img-responsive" src="{{$imag->URL}}" alt="Lights" style="width:300px;height: 250px;">
+          
+        </a>
+        <div class="overlay">
+           
+           <a class="info" href="#">plus de details</a>
+        </div>
+      </div>
+      <div class="caption" style="text-align: center;">
+            <p>{{$imag->description}}</p>
+          </div>
+  </div>
+    </div>  
+    @endforeach 
+    </div>
+</div>
+
+@endsection
+
+
+
+<style type="text/css">
+	.hovereffect {
+  
+  
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  cursor: default;
+}
+
+.hovereffect .overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+}
+
+.hovereffect img {
+  display: block;
+  position: relative;
+  -webkit-transition: all 0.4s ease-in;
+  transition: all 0.4s ease-in;
+}
+
+.hovereffect:hover img {
+  filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feColorMatrix type="matrix" color-interpolation-filters="sRGB" values="0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0 0 0 1 0" /><feGaussianBlur stdDeviation="3" /></filter></svg>#filter');
+  filter: grayscale(1) blur(3px);
+  -webkit-filter: grayscale(1) blur(3px);
+  -webkit-transform: scale(1.2);
+  -ms-transform: scale(1.2);
+  transform: scale(1.2);
+}
+
+.hovereffect h2 {
+  text-transform: uppercase;
+  text-align: center;
+  position: relative;
+  font-size: 17px;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.6);
+}
+
+.hovereffect a.info {
+  display: inline-block;
+  text-decoration: none;
+  padding: 7px 14px;
+  border: 1px solid #fff;
+  margin: 100px 0 0 0;
+  background-color: transparent;
+}
+
+.hovereffect a.info:hover {
+  box-shadow: 0 0 5px #fff;
+}
+
+.hovereffect a.info, .hovereffect h2 {
+  -webkit-transform: scale(0.7);
+  -ms-transform: scale(0.7);
+  transform: scale(0.7);
+  -webkit-transition: all 0.4s ease-in;
+  transition: all 0.4s ease-in;
+  opacity: 0;
+  filter: alpha(opacity=0);
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.hovereffect:hover a.info, .hovereffect:hover h2 {
+  opacity: 1;
+  filter: alpha(opacity=100);
+  -webkit-transform: scale(1);
+  -ms-transform: scale(1);
+  transform: scale(1);
+}
+</style>
+
